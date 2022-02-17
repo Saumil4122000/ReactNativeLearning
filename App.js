5b54a4feae72044aae6acefd52c23a174275c500
Shah Saumil <saumil1220@gmail.com>
2022-02-17 23:28:50 +0530

    "Works on vibrator and toast"

--- App.js
+++ App.js
@@ -1,10 +1,80 @@
 import React from "react";
+import { Text, StyleSheet, Button,StatusBar, View,Vibration,ToastAndroid } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const Separator = () => {
+  return <View style={Platform.OS === "android" ? styles.separator : null} />;
+}
 
 
 const App = () => {
+  const showToast = () => {
+    ToastAndroid.show("A pikachu appeared nearby !", ToastAndroid.LONG,ToastAndroid.TOP);
+  };
+
+  const showToastWithGravity = () => {
+    ToastAndroid.showWithGravity(
+      "All Your Base Are Belong To Us",
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER
+    );
+  };
+
+  const showToastWithGravityAndOffset = () => {
+    ToastAndroid.showWithGravityAndOffset(
+      "A wild toast appeared!",
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+      25,
+      50
+    );
+  };
   return (
-   <Text>HI SAUMIL</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={[styles.header, styles.paragraph]}>Vibration API</Text>
+      <Separator/>
+      <View>
+        <Button title="Vibrate once" onPress={() => Vibration.vibrate()} />
+      </View>
+      <Separator />
+      <Button title="Toggle Toast" onPress={() => showToast()} />
+      <Separator />
+
+      <Button
+        title="Toggle Toast With Gravity"
+        onPress={() => showToastWithGravity()}
+      />
+      <Separator />
+
+      <Button
+        title="Toggle Toast With Gravity & Offset"
+        onPress={() => showToastWithGravityAndOffset()}
+      />
+    </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: StatusBar.currentHeight,
+    padding: 8
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  paragraph: {
+    margin: 24,
+    textAlign: "center"
+  },
+  Seperator:{
+    marginVertical:8,
+    borderBottomColor:"#737373",
+    borderBottomWidth:StyleSheet.hairlineWidth
+  }
+})
 export default App;
