c32d1ca1a52712e028592dc641d2d584ed872b95
Shah Saumil <saumil1220@gmail.com>
2022-02-19 19:20:47 +0530

    "Flatlist Done"

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React from "react";
-
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const App = () => {
+  const arr = ["Saumil", "Shah", "Saumil", "Shah", "I", "am", "Saumil", "Shah"]
   return (
-   <Text>HI SAUMIL</Text>
-  );
+    <View>
+      <FlatList
+        data={arr}
+        renderItem={( item ) => {
+          return (
+            // console.log(item)
+            <Text style={styles.item}>{item.item}</Text>
+          )
+        }}
+        
+      />
+    </View>
+  )
 }
 
-export default App;
+export default App
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    fontSize: 18,
+    fontFamily: 'sans-serif-condensed',
+    color: '#333',
+    textAlign: 'center',
+    backgroundColor: '#eee',
+    marginTop: 5,
+    textTransform: 'uppercase',
+    
+  }
+})
+  
---
+++ readme.md
@@ -0,0 +1,10 @@
+<Text style={{fontFamily: 'normal'}}> normal </Text>
+<Text style={{fontFamily: 'notoserif'}}> notoserif </Text>
+<Text style={{fontFamily: 'sans-serif'}}> sans-serif </Text>
+<Text style={{fontFamily: 'sans-serif-light'}}> sans-serif-light </Text>
+<Text style={{fontFamily: 'sans-serif-thin'}}> sans-serif-thin </Text>
+<Text style={{fontFamily: 'sans-serif-condensed'}}> sans-serif-condensed </Text>
+<Text style={{fontFamily: 'sans-serif-medium'}}> sans-serif-medium </Text>
+<Text style={{fontFamily: 'serif'}}> serif </Text>
+<Text style={{fontFamily: 'Roboto'}}> Roboto </Text>
+<Text style={{fontFamily: 'monospace'}}> monospace </Text>
