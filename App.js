a057dbe6ba65d2f3da9719a8f14ca61890d1c9d7
Shah Saumil <saumil1220@gmail.com>
2022-02-19 21:26:08 +0530

    "Flatlist Done"

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { FlatList, StyleSheet, Text, View, StatusBar,ScrollView } from 'react-native'
 
 const App = () => {
-  const arr = ["Saumil", "Shah", "Saumil", "Shah", "I", "am", "Saumil", "Shah"]
+  const arr = [
+    {
+      key: '1',
+      name: "Saumil"
+    },
+    {
+      key: '2',
+      name: "Shah"
+    },
+    {
+      key: '3',
+      name: "Saumil"
+    },
+    {
+      key: '4',
+      name: "Shah"
+    },
+    {
+      key: '5',
+      name: "I"
+    },
+    {
+      key: '6',
+      name: "am"
+    },
+    {
+      key: '7',
+      name: "Saumil"
+    },
+    {
+      key: '8',
+      name: "Shah"
+    }]
   return (
-    <View>
+    <>
+      <StatusBar barStyle="dark-content" hidden={false} backgroundColor="orange" translucent={true} />
       <FlatList
         data={arr}
-        renderItem={( item ) => {
+        style={styles.listStyle}
+        keyExtractor={(item) => { return item.key }}
+        horizontal={true}
+        // numColumns={2}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => {
           return (
-            // console.log(item)
-            <Text style={styles.item}>{item.item}</Text>
-          )
+            // console.log(item.item.name)
+            <Text style={styles.item}>{item.name}</Text>)
         }}
-        
       />
-    </View>
+    </>
   )
 }
 
@@ -23,15 +59,21 @@ export default App
 
 const styles = StyleSheet.create({
   item: {
-    padding: 10,
-    fontSize: 18,
-    fontFamily: 'sans-serif-condensed',
-    color: '#333',
+    padding: 30,
+    fontSize: 40,
+    backgroundColor: '#f9c2ff',
+    margin: 20,
     textAlign: 'center',
-    backgroundColor: '#eee',
-    marginTop: 5,
-    textTransform: 'uppercase',
-    
-  }
+    fontFamily: 'Roboto',
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 300,
+    height: 150,
+  },
+
+  listStyle: {
+    marginTop: 20,
+    margin:"auto"
+  } 
 })
-  
