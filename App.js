ebbaff893314f1261beca386b8dfa965ad9c21dc
Shah Saumil <saumil1220@gmail.com>
2022-02-20 18:25:30 +0530

    "Netflix Card Created"

--- App.js
+++ App.js
@@ -1,98 +1,43 @@
+import { useFonts } from '@expo-google-fonts/josefin-sans'
 import React from 'react'
-import { FlatList, StyleSheet, Image, StatusBar, Text, View, ToastAndroid, TouchableOpacity, Alert, Platform } from 'react-native'
-
+import { StyleSheet, Image, StatusBar, Text, View, Button, Linking, TouchableOpacity } from 'react-native'
+import { JosefinSans_100Thin, JosefinSans_700Bold,JosefinSans_400Regular } from '@expo-google-fonts/josefin-sans'
+import AppLoading from 'expo-app-loading'
 const App = () => {
-  const arr = [
-    {
-      key: '1',
-      uri: require("./assets/images/1.png")
-    },
-    {
-      key: '2',
-      uri: require("./assets/images/2.png"),
 
-    },
-    {
-      key: '3',
-      uri: require("./assets/images/3.png"),
+  let [fontsLoaded] = useFonts({
+    JosefinSans_100Thin,
+    JosefinSans_700Bold,
+    JosefinSans_400Regular
+  })
 
-    },
-    {
-      key: '4',
-      uri: require("./assets/images/4.png"),
-
-    },
-    {
-      key: '5',
-      uri: require("./assets/images/5.png"),
-    },
-    {
-      key: '6',
-      uri: require("./assets/images/1.png")
-    },
-    {
-      key: '7',
-      uri: require("./assets/images/2.png"),
-
-    },
-    {
-      key: '8',
-      uri: require("./assets/images/3.png"),
-
-    },
-    {
-      key: '9',
-      uri: require("./assets/images/4.png"),
-
-    },
-    {
-      key: '10',
-      uri: require("./assets/images/5.png"),
-    }
-  ]
-
-
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
 
   return (
     <>
       <StatusBar barStyle="dark-content" hidden={false} backgroundColor="orange" translucent={true} />
       <View style={styles.container}>
-        <FlatList
-          data={arr}
-          // contentContainerStyle={styles.listStyle}
-          keyExtractor={(item) => { return item.key }}
-          horizontal={true}
-          // numColumns={3}
-          // style={{ flex: 1 }}
-          // inverted={true}
-          showsHorizontalScrollIndicator={false}
-          // showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.imagecard}
+        <Text style={styles.header}> Netflix Card</Text>
 
-              onMouseOver={() => { Platform.OS === "web" ?
-              
-              alert(item.key) : console.log("HI") }}
 
-                onTouchEnd={() => {
-                  Platform.OS === "android" ? ToastAndroid.show(item.key, ToastAndroid.SHORT) : Alert.alert(item.key)
-                }}>
-                <Image source={item.uri} style={styles.imageStyle} />
-              </View>
-            )
-          }}
-        />
-      </View>
+        <View style={styles.poster}>
+          <Image source={require("./assets/123.jpg")} style={styles.imgStyle} />
 
-      <TouchableOpacity onPress={() => {
-        Platform.OS === 'android' ? Alert.alert("Alert") : alert("Alert")
-      }}
-        style={{ position: "absolute", bottom: 10, left: 150 }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Click Me</Text>
+
+          <View style={styles.poster__info}>
+            <Text style={styles.poster__title}>All of us Dead</Text>
+            <Text style={styles.poster__text}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolores ipsa earum obcaecati voluptatem saepe quae eos cumque! In, ab.
+            </Text>
+          </View>
+
+          <Button title="Watch Now" onPress={() =>
+            Linking.openURL('https://www.netflix.com/watch/80109847')}
+          />
         </View>
-      </TouchableOpacity>
+      </View>
     </>
   )
 }
@@ -100,46 +45,43 @@ const App = () => {
 export default App
 
 const styles = StyleSheet.create({
-  imageStyle: {
-    width: 150,
-    height: 150,
-    marginTop: 30,
-    // marginVertical: "auto",
-  },
   container: {
-    flex: 1,
-    paddingTop: 22,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  imagecard: {
-    height: 230,
-    borderWidth: 1,
-    marginRight: 20,
-    marginLeft: 3,
-    marginTop: 40,
-    backgroundColor: 'transparent',
-    borderRadius: 9,
-    // alignSelf:'flex-start',
-    backgroundColor: 'lightgrey',
-  },
-  listStyle: {
-    display: 'flex',
+    padding: 50,
     justifyContent: 'center',
     alignItems: 'center',
-    borderWidth: 1,
-    backgroundColor: 'skyblue',
   },
-  button: {
-    width: 100,
-    height: 50,
-    backgroundColor: "red",
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    fontFamily: "sans-serif-medium",
-    fontWeight: "bold",
-    marginTop: 20
+  header: {
+    fontSize: 30,
+    marginBottom: 20,
+    fontFamily: 'JosefinSans_700Bold',
+  },
+  imgStyle: {
+    width: "100%",
+    height: undefined,
+    aspectRatio: 1,
+    borderRadius: 5
+  },
+  poster: {
+    width: 250,
+    borderWidth: 2,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  poster__info: {
+    alignItems: 'center',
+    marginVertical: 20
+  },
+  poster__title: {
+    fontSize: 20,
+    marginBottom: 10,
+    fontFamily: 'JosefinSans_400Regular',
+  },
+  poster__text: {
+    color: '#999',
+    paddingHorizontal: 20,
+    marginBottom: 10,
+    fontSize: 16,
+    fontFamily: 'JosefinSans_400Regular',
+    textAlign: 'center'
   }
 })
-
